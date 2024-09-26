9d7d51c7eea5a8cd89ee7b31e4d12ddc8b17c144
abhipawar01 <abhishekpawar969696@gmail.com>
2024-09-26 21:39:14 +0530

    Create basic Book class structure

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,15 @@
 public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    // TODO: Add getters and setters
 }
