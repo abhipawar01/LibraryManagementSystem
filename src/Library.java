67b381a34799f34b546a80ad8410835bedf2d696
abhipawar01 <abhishekpawar969696@gmail.com>
2024-09-26 21:45:19 +0530

    Implemented addBook method in Library class

--- src/Library.java
+++ src/Library.java
@@ -7,6 +7,12 @@ public class Library {
     public Library() {
         this.books = new HashMap<>();
     }
+    public void addBook(String isbn, String title, String author, int publicationYear) {
+        if (books.containsKey(isbn)) {
+            throw new IllegalArgumentException("Book with ISBN " + isbn + " already exists.");
+        }
+        books.put(isbn, new Book(isbn, title, author, publicationYear));
+    }
 
     // TODO: Add methods for adding, borrowing, and returning books
 }
