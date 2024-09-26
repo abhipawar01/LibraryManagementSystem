0d8985fe696a7f68204574fa71e2c2783d89c52c
abhipawar01 <abhishekpawar969696@gmail.com>
2024-09-26 21:43:19 +0530

    Created basic Library class structure

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Library {
+    private Map<String, Book> books;
+
+    public Library() {
+        this.books = new HashMap<>();
+    }
+
+    // TODO: Add methods for adding, borrowing, and returning books
 }
