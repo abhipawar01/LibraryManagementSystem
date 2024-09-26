10fb8e71ae46d01a7275533374b79c648f30c438
abhipawar01 <abhishekpawar969696@gmail.com>
2024-09-26 21:53:15 +0530

    Created basic LibraryManagementSystem class structure

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -1,3 +1,12 @@
-public class LibraryManagementSystem {
+import java.util.Scanner;
 
+public class LibraryManagementSystem {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Library library = new Library();
+
+    public static void main(String[] args) {
+        // TODO: Implement main menu and user interaction
+    }
+
+    // TODO: Add methods for user interactions
 }
